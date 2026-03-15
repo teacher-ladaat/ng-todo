448de688720ca35d8adee1eae0de1f198d1b8537
angular-teacher <angularteacher@gmail.com>
2026-03-15 12:27:49 +0200

    feat: add TodoList component with template and styles, and integrate into app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Header } from "./components/header/header";
+import { TodoList } from './components/todo-list/todo-list';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, TodoList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/components/todo-list/todo-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  imports: [],
+  templateUrl: './todo-list.html',
+  styleUrl: './todo-list.scss',
+})
+export class TodoList {}
