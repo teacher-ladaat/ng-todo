0275e5f93ecfca2e555b1b2aa187f89ffc611556
angular-teacher <angularteacher@gmail.com>
2026-03-15 13:21:09 +0200

    feat: add Todo component with template, styles, and tests

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoComponent } from './components/todo/todo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, TodoListComponent],
+  imports: [HeaderComponent, TodoListComponent, TodoComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/components/todo/todo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  imports: [],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.scss',
+})
+export class TodoComponent {}
